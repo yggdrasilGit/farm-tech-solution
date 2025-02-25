44b821bce09ee72a326f4b2ab946ab1f0061f8b2
yggdrasilGit <168605322+yggdrasilGit@users.noreply.github.com>
2025-02-25 11:12:23 -0300

    Modificado e colocado explicação  na documentaçãoo. n
    coloque somente explicacao na documenteacao

--- input_data/culture_type.py
+++ input_data/culture_type.py
@@ -1,8 +1,14 @@
-
-
-
-
 def escolher_cultura():
+    """
+    Permite ao usuário selecionar duas culturas distintas para trabalhar.
+
+    O usuário deve inserir os nomes das culturas desejadas (exemplo: soja, milho).
+    Se uma cultura já tiver sido escolhida, será solicitado que escolha uma diferente.
+    O processo continua até que duas culturas distintas sejam selecionadas.
+
+    Returns:
+        list: Uma lista contendo as duas culturas escolhidas pelo usuário.
+    """
     culturas = []
     while len(culturas) < 2:
         cultura = input("Digite a cultura que deseja trabalhar (soja/milho ou qualquer outra): ").lower()
--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 from input_data.culture_type import escolher_cultura
 
 def menu():
+    """
+    Exibe o menu principal do programa e chama a função para escolher culturas.
+
+    A função `escolher_cultura()` permite ao usuário selecionar duas culturas
+    distintas para trabalhar e armazena a escolha.
+
+    Returns:
+        None
+    """
     cultura = escolher_cultura()
-    
 
 if __name__ == "__main__":
     menu()
